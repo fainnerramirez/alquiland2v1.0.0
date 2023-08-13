fc7df4ae8803ac3de8e0677b6d9b38cd74ea4325
Fainner Ramirez <framirez@godoycordoba.com>
2023-08-12 20:17:30 -0500

    Update commit

--- src/components/CardsRoles.jsx
+++ src/components/CardsRoles.jsx
@@ -1,9 +1,65 @@
 import React from 'react'
-import { Card, Stack, CardBody, Heading, Text, Image, Divider, CardFooter, Button, ButtonGroup } from "@chakra-ui/react"
+import { 
+  Card, 
+  Stack, 
+  CardBody, 
+  Heading, 
+  Text, 
+  Image, 
+  Divider, 
+  CardFooter, 
+  Button, 
+  ButtonGroup, 
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter
+} from "@chakra-ui/react"
 import { AiOutlineArrowRight } from 'react-icons/ai'
 import {Link as LinkRouter} from "react-router-dom";
 
+const StudentsButtonModal = ({role}) => {
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  return (
+    <>
+      <Button 
+         variant='solid' 
+         colorScheme='pink' 
+         size={'md'}  
+         width={"full"} 
+         rightIcon={<AiOutlineArrowRight />}
+        onClick={onOpen}
+      >
+        Soy  {role}
+      </Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam, laudantium!
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant='ghost'>Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
+
 export const CardsRoles = ({role, image, link}) => {
+
   return (
     <Card maxW='sm' m="10px">
       <CardBody>
@@ -17,11 +73,7 @@ export const CardsRoles = ({role, image, link}) => {
       </CardBody>
       <Divider />
       <CardFooter>
-        <Button variant='solid' colorScheme='pink' size={'md'}  width={"full"} rightIcon={<AiOutlineArrowRight />}>
-          <LinkRouter to={link}>
-              Soy {role}
-          </LinkRouter>
-        </Button>
+        <StudentsButtonModal role={role} />
       </CardFooter>
     </Card>
   )
