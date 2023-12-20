e315b43404f4326fc50f54b6a24616ac17121f9c
Fainner Ramirez <framirez@godoycordoba.com>
2023-12-19 20:27:36 -0500

    updtae commit

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -30,7 +30,7 @@ const Gallery = () => {
         <>
             <Navbar />
             <Heading textAlign={'center'} pt={10} pb={10}>Anuncios publicados</Heading>
-            <Grid templateColumns="repeat(5, 1fr)"
+            <Grid templateColumns={{base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)', lg: 'repeat(3, 1fr)', xl: 'repeat(5, 1fr)'}}
                 gap="10"
                 width={'95%'}
                 margin={'auto'}>
